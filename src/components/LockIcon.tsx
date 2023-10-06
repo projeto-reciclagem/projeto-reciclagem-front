c27a5e3aec88f2f049b0d28b53a80586558047c7
vinicius-dosreis-pereira <vinicius.pereira@sptech.school>
2023-10-06 16:46:49 -0300

    UPDATE: alterações necessárias na dev

--- src/components/LockIcon.tsx
+++ src/components/LockIcon.tsx
@@ -1,4 +1,4 @@
-import { LockIcon } from "lucide-react";
+import { LockIcon } from "lucide-react"; 
 
 const IconLock = () => {
     return <LockIcon color="#1a503f" size={56} className="flex relative top-6 left-9 bg-marine-50" />
