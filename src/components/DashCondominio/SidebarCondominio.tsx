907769b577ddff87783946f0deaf239d0fbce122
vinicius-dosreis-pereira <vinicius.pereira@sptech.school>
2023-11-25 15:16:58 -0300

    UPDATE: Alterações na Landing Page do condominio

--- src/components/DashCondominio/SidebarCondominio.tsx
+++ src/components/DashCondominio/SidebarCondominio.tsx
@@ -1,7 +1,8 @@
 import logo from '@/assets/eco-system-logo.svg'
+import graphic from '@/assets/lp_condominio_graphic.jpg'
 import { LogOut } from 'lucide-react'
 import { useState } from 'react'
-import { KanbanSquare, MessageCircle, ClipboardList, BarChart4, Cog} from 'lucide-react'
+import { KanbanSquare, MessageCircle, ClipboardList, BarChart4, Cog } from 'lucide-react'
 
 interface SideBarCondominioProps {
     children: React.ReactNode
@@ -9,51 +10,49 @@ interface SideBarCondominioProps {
 
 export default function SideBarCondominio({ children }: SideBarCondominioProps) {
 
-    const [menuus, setMenu] = useState(true)
-    const [activeItem, setActiveItem] = useState('')
-
-    const toggleMenu = () => {
-        setMenu(!menuus)
-    }
-
-    const handleSetActiveItem = (item: string) => {
-        setActiveItem(item)
-    }
-
     return (
         <>
-            <main className='flex p-2 gap-4'>
-                <div className='flex w-56 justify-between h-5/6 self-center justify-center flex-shrink-0 rounded-3xl bg-marine-700'>
-                    <nav className='flex flex-col items-center'>
+            <main className='flex p-2 gap-4 bg-marine-50'>
+                <div className='flex w-56 justify-between h-max self-center flex-shrink-0 rounded-3xl bg-marine-700 p-6'>
+                    <nav className='flex flex-col items-center self-center'>
                         <img src={logo} alt="" className='w-44 h-12' />
-                        <div className="flex w-11/12 flex-col items-center gap-5 pb-5">
+                        <div className="flex w-11/12 flex-col items-center gap-2 pb-4">
                             <div className="h-32 w-32 rounded-full bg-moss-green-50" />
                             <p className="text-2xl text-moss-green-50">Olá, User!</p>
                         </div>
-                        <div className='flex flex-col p-4 gap-6'>
-                            <div className='flex flex-row items-center gap-4'>
-                                <KanbanSquare className='w-10 h-10 '></KanbanSquare>
+                        <div className='flex flex-col p-2 gap-4 bg-moss-green-300 rounded-lg'>
+                            <div className='flex flex-row h-16 gap-4 justify-center items-center bg-marine-300 w-40 p-2'>
+                                <KanbanSquare className='w-10 h-10 text-marine-900'></KanbanSquare>
                                 <button className=' text-marine-900'>Overview</button>
                             </div>
-                            <div className='flex flex-row items-center gap-4'>
-                                <MessageCircle className='w-10 h-10 '></MessageCircle>
+                            <div className='flex flex-row h-16 gap-4 justify-center items-center w-40 p-2'>
+                                <MessageCircle className='w-10 h-10 text-marine-900'></MessageCircle>
                                 <button className=' text-marine-900'>Chat</button>
                             </div>
-                            <div className='flex flex-row items-center gap-4'>
-                                <ClipboardList className='w-10 h-10 '></ClipboardList>
+                            <div className='flex flex-row h-16 gap-4 justify-center items-center w-40 p-2'>
+                                <ClipboardList className='w-10 h-10 text-marine-900'></ClipboardList>
                                 <button className=' text-marine-900'>Pedidos</button>
                             </div>
-                            <div className='flex flex-row items-center gap-4'>
-                                <BarChart4 className='w-10 h-10 '></BarChart4>
+                            <div className='flex flex-row h-16 gap-4 justify-center items-center w-40 p-2'>
+                                <BarChart4 className='w-10 h-10 text-marine-900'></BarChart4>
                                 <button className=' text-marine-900'>Relatório</button>
                             </div>
-                            <div className='flex flex-row items-center gap-4'>
-                                <Cog className='w-10 h-10 '></Cog>
-                                <button className=' text-marine-900'>Config</button>
+                            <div className='flex flex-row h-16 gap-4 justify-center items-center w-40 p-2'>
+                                <Cog className='w-10 h-10 text-marine-900'></Cog>
+                                <button className=' text-marine-900'>Configurações</button>
                             </div>
+                        </div>
+                        <div className="flex p-2 w-44 h-12 items-center mt-5 justify-center rounded-lg bg-moss-green-300">
+                            <p className="flex select-none items-center gap-6 text-xl">
+                                <LogOut size={28} className="text-marine-900" />
+                                <p className="text-marine-900 lative cursor-pointer transition-all duration-100">
+                                    Sair
+                                </p>
+                            </p>
                         </div>
                     </nav>
                 </div>
+                
             </main>
         </>
     )
--- src/pages/dash-condominio/LandingPageCond.tsx
+++ src/pages/dash-condominio/LandingPageCond.tsx
@@ -4,78 +4,31 @@ import { useState } from 'react'
 
 export default function LandingPageCond() {
 
-    const [selected1, setSelected1] = useState(true)
-    const [selected2, setSelected2] = useState(false)
-    const [selected3, setSelected3] = useState(false)
-
-    const toggleSelected = () => {
-        setSelected1(true)
-        if (selected2) setSelected2(false)
-        if (selected3) setSelected3(false)
-    }
-    const toggleSelected2 = () => {
-        setSelected2(true)
-        if (selected1) setSelected1(false)
-        if (selected3) setSelected3(false)
-    }
-    const toggleSelected3 = () => {
-        setSelected3(true)
-        if (selected1) setSelected1(false)
-        if (selected2) setSelected2(false)
-    }
-
-    return (
-        <>
-      <SideBarCondominio>
-        <div className="mx-auto flex h-[30vh] w-11/12 items-center justify-between gap-5 bg-transparent">
-          <div className="flex h-[200px] w-[300px] flex-col items-center justify-center gap-5 rounded-xl bg-moss-green-50 p-4 text-center">
-            <p className="text-lg">Valor total no último mês</p>
-            <span>R$ 60,00</span>
+  return (
+    <>
+      <main className="flex flex-row justify-center items-center bg-marine-50">
+        <SideBarCondominio>
+        </SideBarCondominio>
+        <div className='flex flex-col items-start p-4 self-stretch w-5/6 h-5/6 gap-3 bg-marine-700 rounded-2xl mt-2'>
+          <div className='flex justify-between items-center self-stretch'>
+            <div className='flex flex-col w-56 h-36 p-3 justify-center items-center gap-2 self-stretch rounded-xl border-2 border-solid border-moss-green-300 bg-moss-green-50'>
+              <p className='text-xl text-marine-900'>Valor total (Último mês)</p>
+              <p className='text-4xl text-moss-green-500'>R$ 60,00</p>
+            </div>
+            <div className='flex flex-col w-56 h-36 p-3 justify-center items-center gap-2 self-stretch rounded-xl border-2 border-solid border-moss-green-300 bg-moss-green-50'>
+              <p className='text-xl text-marine-900'>Coletas realizadas</p>
+              <p className='text-4xl text-moss-green-500'>12</p>
+              <p className='text-base text-marine-900'>Último mês</p>
+            </div>
+            <div className='flex flex-col w-56 h-36 p-3 justify-center items-center gap-2 self-stretch rounded-xl border-2 border-solid border-moss-green-300 bg-moss-green-50'>
+              <p className='text-xl text-marine-900'>Última coleta feita</p>
+              <p className='text-4xl text-moss-green-500'>7 dias atrás</p>
+            </div>
           </div>
-          <div className="flex h-[200px] w-[300px] flex-col items-center justify-center gap-5 rounded-xl bg-moss-green-50 p-4 text-center">
-            <p className="text-lg">Coletas solicitadas</p>
-            <span>12</span>
-          </div>
-          <div className="flex h-[200px] w-[300px] flex-col items-center justify-center gap-5 rounded-xl bg-moss-green-50 p-4 text-center">
-            <p className="text-lg">Material mais reciclado na última semana</p>
-            <span>PET - 42ton</span>
+          <div className='flex self-center justify-center w-[1080px] h-[560px] bg-marine-50 rounded-xl'>
           </div>
         </div>
-        <div className="relative mx-auto flex h-[65vh] w-11/12 gap-3">
-          <div
-            className={`${
-              selected1
-                ? `left-0 top-0 h-full w-8/12`
-                : `h-[31.8vh] w-[32.3%] ${
-                    selected2 ? 'right-0 top-0' : 'bottom-0 right-0'
-                  }`
-            } trnasition-all absolute cursor-pointer select-none rounded-lg bg-rose-600 duration-200`}
-            onClick={toggleSelected}
-          >
-            <h4 className="text-center">Reciclagem Semanal</h4>
-          </div>
-
-          <div
-            className={`${
-              selected2 ? 'h-full w-8/12' : 'right-0 top-0 h-[31.8vh] w-[32.3%]'
-            } trnasition-all absolute cursor-pointer  select-none rounded-lg bg-teal-400 duration-200`}
-            onClick={toggleSelected2}
-          >
-            <h4 className="text-center">Reciclagem Semanal</h4>
-          </div>
-
-          <div
-            className={`${
-              selected3
-                ? 'h-full w-8/12'
-                : 'bottom-0 right-0 h-[31.8vh] w-[32.3%]'
-            } trnasition-all absolute cursor-pointer  select-none rounded-lg bg-moss-green-50 duration-200`}
-            onClick={toggleSelected3}
-          >
-            <h4 className="text-center">Reciclagem Semanal</h4>
-          </div>
-        </div>
-      </SideBarCondominio>
+      </main>
     </>
-    )
+  )
 }
